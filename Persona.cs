fb7744343fca87872941e79653233c564b2eba7a
Nicolás Gomez <nco_0.sys@hotmail.com>
2019-04-15 16:23:20 -0400

    ejercio de clases

---
+++ Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class Persona : Mamifero, IComportamientoHumano, IProgramar
+    {
+        public void Conversar(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EscribeHelloWorld()
+        {
+            Console.WriteLine("print('hello world')");
+        }
+
+        public void Pensar()
+        {
+        }
+
+        public void Programa()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+
+    public class Mamifero
+    {
+        public virtual void pensar()
+        {
+            Console.WriteLine("Pensamiento instintivo básico para sobrevivir...");
+        }
+    }
+
+    class Humano : Mamifero
+    {
+        public override void pensar()
+        {
+            Console.WriteLine("Pensamiento inteligente (¿?)");
+        }
+
+        public void Hablar()
+        {
+
+        }
+    }
+
+    class Gorila : Mamifero
+    {
+        public override void pensar()
+        {
+            Console.WriteLine("Pensamiento más avanzado, puedo hacer cosas más complejas que muchos animales");
+        }
+    }
+
+    class Caballo : Mamifero
+    {
+
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Persona miPersona = new Persona();
+            IComportamientoHumano c = new Persona();
+            Programar p = new Persona();
+            p.EscribeHelloWorld();
+        }
+    }
+}
